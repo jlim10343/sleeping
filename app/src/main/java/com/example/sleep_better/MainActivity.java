b76b555b7b8ffef37b30794a5c4f623521365fa3
jlim10343 <aiwen.joy.lim@gmail.com>
2019-05-25 14:56:57 -0400

    made bottom nav work

--- app/src/main/java/com/example/sleep_better/MainActivity.java
+++ app/src/main/java/com/example/sleep_better/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.sleep_better;
 
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean loadFragment (Fragment fragment)
+    {
+        if (fragment != null)
+        {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()) {
+                case R.id.accelerate:
+                    selectedFragment = new AccelerometerFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+
+            return false;
+        }
+    };
 }
