11b4300673444b9404f3a05884df5aca0a6296e9
bleaugh <ayc228@gmail.com>
2019-05-25 13:01:13 -0700

    added sound functionality

--- app/src/main/java/com/example/sleep_better/MainActivity.java
+++ app/src/main/java/com/example/sleep_better/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.sleep_better;
 
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer audio;
+
 
     private boolean loadFragment (Fragment fragment)
     {
@@ -48,4 +54,32 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
+
+    public void playAudio(View v) {
+        if(audio == null) {
+            audio = MediaPlayer.create(this,R.raw.catsound);
+            audio.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+        audio.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        audio.release();
+    }
+
+    private void stopPlayer()
+    {
+        if(audio != null) {
+            audio.release();
+            audio = null;
+            Toast.makeText(this,"YEEEE",Toast.LENGTH_SHORT).show();
+        }
+    }
 }
