a7f645618305878c7a22688e3a4e5b381b2adb34
supriyo11 <supriyo.rana@live.com>
2019-05-25 14:44:04 -0400

    check if audio plays

--- app/src/main/java/com/example/sleep_better/MainActivity.java
+++ app/src/main/java/com/example/sleep_better/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.sleep_better;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer audio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        audio=MediaPlayer.create(MainActivity.this,R.raw.catSound);
     }
+
+    public void playAudio(View v) {
+        audio.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        audio.release();
+    }
+
+
 }
