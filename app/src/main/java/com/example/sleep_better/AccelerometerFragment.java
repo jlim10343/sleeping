0429cc12919378037a59452f5e17ba91f67b80b3
jlim10343 <aiwen.joy.lim@gmail.com>
2019-05-25 15:22:33 -0400

    acceleration

--- app/src/main/java/com/example/sleep_better/AccelerometerFragment.java
+++ app/src/main/java/com/example/sleep_better/AccelerometerFragment.java
@@ -1,5 +1,10 @@
 package com.example.sleep_better;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEvent;
+import android.hardware.TriggerEventListener;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +12,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AccelerometerFragment extends Fragment {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private TriggerEventListener triggerEventListener;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.accfragment, container, false);
+        final View view = inflater.inflate(R.layout.accfragment, container, false);
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION);
+
+        triggerEventListener = new TriggerEventListener() {
+            @Override
+            public void onTrigger(TriggerEvent event) {
+                TextView t = view.findViewById(R.id.textView);
+                String s = "shook";
+                t.setText(s);
+            }
+        };
+
+        sensorManager.requestTriggerSensor(triggerEventListener, sensor);
 
         return view;
     }
